a2f481363f2641646905b26139ccdd1eb3dcea2b
Daniil Kiryanchuk <job.kiryanchuk@gmail.com>
2022-06-18 17:37:33 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Даниил Кирьянчук on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
